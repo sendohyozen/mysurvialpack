0b5dea6c3ab5ce7f5862f0fa58a9c167c6966997
李颖 <liying@liyingdBPNO2125.AX88U>
2024-05-04 22:29:01 +0800

    my_survial_0.9_20240504

--- R/survival_cutoff.R
+++ R/survival_cutoff.R
@@ -7,7 +7,7 @@
 #' @param positive.lab positive label defines event occurs; such as 'Dead'
 #' @param minprop minimal proption to prevent unbanlanced grouping
 #'
-#' @return a best cutoff surv fit object
+#' @return a best cutoff surv fit cutoff value
 #' @export
 #'
 #' @examples surv_bestcutoff(surv.data = sur_data, time = "days_to_last_follow_up", vital = "vital_status", numeric.varName = "total_perMB", positive.lab = 'Dead')
@@ -33,5 +33,5 @@ surv_bestcutoff <- function(surv.data, time, vital, numeric.varName, positive.la
                                                     minprop = minprop,     # minimal proption to prevent unbanlance
                                                     progressbar = TRUE)  # show progressbar
 
-    return(best_threshold_surv)
+    return(best_threshold_surv$cutpoint$cutpoint)
 }
